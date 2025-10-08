5f6d2f76de70d953e38e0fcc5dbad495ba223cd8
Nathan Henrique <rm95268@estudante.fieb.edu.br>
2025-10-08 19:30:12 -0300

    consertado

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById('root')
+if (!root) {
+  console.error('Root element not found')
+} else {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
