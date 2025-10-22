fa42d552d98ccc4671ef0a63c7d185b35ee106f1
daniel-santoss <seu-email-do-github>
2025-10-21 21:09:18 -0300

    Fix: Convert build script to ES modules

--- build.js
+++ build.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-const { spawn } = require('child_process');
-const path = require('path');
+import { spawn } from 'child_process';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const vitePath = path.join(__dirname, 'node_modules', 'vite', 'bin', 'vite.js');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const vitePath = join(__dirname, 'node_modules', 'vite', 'bin', 'vite.js');
 const child = spawn('node', [vitePath, 'build'], { stdio: 'inherit' });
 
 child.on('close', (code) => {
